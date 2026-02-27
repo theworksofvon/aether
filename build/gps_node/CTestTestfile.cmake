c6c90cf3e580f4bb981ff17374a432ef944ef521
theworksofvon <davontaejackson667@gmail.com>
2026-02-27 14:17:47 -0500

    chore: remove build artifacts from tracking

--- build/gps_node/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /aether/src/gps_node
-# Build directory: /aether/build/gps_node
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
